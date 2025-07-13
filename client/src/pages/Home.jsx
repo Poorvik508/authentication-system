eba93390cc759d7d138499f9bf720c4170e5ad15
Poorvik P <poorvikp508@gmail.com>
2025-07-13 20:02:39 +0530

    home page done

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,11 +3,11 @@ import Navbar from "../components/Navbar"
 import Header from "../components/Header"
 const Home = () => {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-[url('/bg_img.png')] bg-cover bg-center">
       <Navbar />
-      <Header/>
+      <Header />
     </div>
-  )
+  );
 }
 
 export default Home
--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { assets } from "../assets/assets"
 
 const Login = () => {
+   const [state,setstate]=useState('Sign Up')
   return (
-    <div>
-      <h1>Login Page</h1>
+    <div className="flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-100 to-purple-400">
+      <img
+        src={assets.logo}
+        alt=""
+        className="absolute left-5 sm:left-20 top-5 w-28 sm:w-28 cursor-pointer"
+      />
+      <div className="bg-slate-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm">
+        <h2 className="text-3xl font-semibold text-white text-center mb-3">{state === "Sign Up" ? "Create Account" : "Login "}</h2>
+        <p className="text-center text-sm mb-6">
+          {state === "Sign Up"
+            ? "Create Your Account"
+            : "Login To Your Account!"}
+        </p>
+        <form >
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.person_icon} alt="" />
+            <input className="bg-transparent outline-none" type="text" placeholder="Full Name" required />
+            
+
+            
+          </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.mail_icon} alt="" />
+            <input className="bg-transparent outline-none" type="email" placeholder="Email Id" required />
+
+
+            
+          </div>
+          <div className="mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]">
+            <img src={assets.lock_icon} alt="" />
+            <input className="bg-transparent outline-none" type="password" placeholder="Password" required />
+
+
+            
+          </div>
+          <p className="mb-4 text-indigo-500 cursor-pointer">Forgot Password?</p>
+          <button className="text-white font-bold  w-full py-2.5 rounded-full bg-gradient-to-r from-indigo-500 to-indigo-900">{state }</button>
+        </form>
+      </div>
     </div>
-  )
+  );
 }
 
 export default Login
